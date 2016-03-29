2887a5339804e8a690796cbc037e32d9ead25c19
Purwa Darozatun <purwadarozatunakis@gmail.com>
2016-03-29 10:40:34 +0700

    Add Usage of root object if doest fill mode in config

--- app-config/index.js
+++ app-config/index.js
@@ -1,4 +1,9 @@
 var config = require('../config.json')
 module.exports = function(mode) {
-    return config[mode || process.argv[2] || 'production'] || config.local;
+    
+    if(( typeof config != 'undefined') ){
+        return config[mode || process.argv[2] || 'production'] || config;
+    }else{
+        console.log("Please Fill Config File");
+    }
 }
